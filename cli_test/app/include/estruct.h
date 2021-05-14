2a134aa8307dba4c755e8944a8b8c2ff77bb8395
Greg Martin <66388179+gmartin102@users.noreply.github.com>
2021-05-14 07:06:21 -0700

    updating with efpga rw/multiplier tests (#6)
    Co-authored-by: raj mailapalli <rmailapalli@quicklogic.com>

--- cli_test/app/include/estruct.h
+++ cli_test/app/include/estruct.h
@@ -1,9 +1,31 @@
-/*
- * estruct.c
+/*==========================================================
+ * Copyright 2021 QuickLogic Corporation
  *
- *  Created on: Jan 19, 2021
- *      Author: gregmartin
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *==========================================================*/
+
+/*==========================================================
+ *
+ *    File   : estruct.h
+ *    Purpose: To test efpga block
+ *    Author: gregmartin
+ *
+ *=========================================================*/
+
+#define EFPGA_DEBUG 0
+#define APB_SOC_CTRL_BASE_ADDR 0x1A104000
+#define EFPGA_BASE_ADDR 0x1A300000
+
 
 typedef struct {
 	volatile unsigned int CFG_REG_LO; //         6'h0
