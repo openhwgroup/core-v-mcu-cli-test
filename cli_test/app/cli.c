5d417cde2c277305526abfeb441fa35797e0705e
raj mailapalli <rmailapalli@quicklogic.com>
2021-06-01 15:14:22 +0530

    Separating and clean up of i2c tests

--- cli_test/app/cli.c
+++ cli_test/app/cli.c
@@ -29,7 +29,7 @@
 #include "libs/utils/include/dbg_uart.h"
 #include "hal/include/hal_apb_soc_ctrl_regs.h"
 
-#include "drivers/include/udma_i2cm_driver.h"
+//#include "drivers/include/udma_i2cm_driver.h"
 #include "drivers/include/udma_uart_driver.h"
 #include "hal/include/hal_fc_event.h"
 
@@ -40,9 +40,8 @@ const struct cli_cmd_entry mem_functions[];
 const struct cli_cmd_entry mem_tests[];
 extern const struct cli_cmd_entry io_functions[];
 extern const struct cli_cmd_entry gpio_functions[];
-const struct cli_cmd_entry i2cm0_functions[];
-const struct cli_cmd_entry i2cm0_tests[];
-const struct cli_cmd_entry i2cm1_functions[];
+extern const struct cli_cmd_entry i2cm0_functions[];
+extern const struct cli_cmd_entry i2cm1_functions[];
 extern const struct cli_cmd_entry efpga_cli_tests[];
 extern const struct cli_cmd_entry qspi_cli_tests[];
 
@@ -64,15 +63,6 @@ static void mem_poke_8(const struct cli_cmd_entry *pEntry);
 // MEM tests
 static void mem_check(const struct cli_cmd_entry *pEntry);
 
-
-// I2CM0 functions
-static void i2cm_readbyte(const struct cli_cmd_entry *pEntry);
-static void i2cm_writebyte(const struct cli_cmd_entry *pEntry);
-static void i2cm_reset(const struct cli_cmd_entry *pEntry);
-static void i2c_temp(const struct cli_cmd_entry *pEntry);
-static void i2c_read_dev_id(const struct cli_cmd_entry *pEntry);
-// I2CM tests
-static void i2cm_singlebyte_test(const struct cli_cmd_entry *pEntry);
 
 // Main menu
 const struct cli_cmd_entry my_main_menu[] = {
@@ -127,33 +117,6 @@ const struct cli_cmd_entry mem_tests[] =
 		CLI_CMD_TERMINATE()
 };
 
-// I2CM0 menu
-const struct cli_cmd_entry i2cm0_functions[] =
-{
-
-  CLI_CMD_WITH_ARG( "readbyte", 	i2cm_readbyte,	0, "i2c_addr reg_addr 			-- read register" ),
-  CLI_CMD_WITH_ARG( "writebyte", 	i2cm_writebyte,	0, "i2c_addr reg_addr value 	-- read register" ),
-  CLI_CMD_SUBMENU ( "test", 		i2cm0_tests, 		"tests" ),
-  CLI_CMD_TERMINATE()
-
-};
-
-const struct cli_cmd_entry i2cm0_tests[] =
-{
-
-  CLI_CMD_WITH_ARG( "singlebyte", 	i2cm_singlebyte_test,	0, "i2c_addr reg_addr	-- writes 0xA5 and then 0x5A to register and checks result" ),
-  CLI_CMD_TERMINATE()
-
-};
-
-// I2CM1 menu
-const struct cli_cmd_entry i2cm1_functions[] =
-{
-		CLI_CMD_WITH_ARG( "readbyte", i2cm_readbyte,	1, "i2c_addr reg_addr 	-- read register" ),
-		CLI_CMD_SIMPLE ( "temp", i2c_temp,				   "read on board temperature"),
-		CLI_CMD_SIMPLE ( "dev_id", i2c_read_dev_id,		    "read i2c device id"),
-		CLI_CMD_TERMINATE()
-};
 
 // MISC functions
 static void misc_info(const struct cli_cmd_entry *pEntry)
@@ -318,113 +281,4 @@ static void mem_poke_8(const struct cli_cmd_entry *pEntry)
 	CLI_uint32_required( "value", &xValue);
 	*pAddr = xValue;
 	dbg_str("<<DONE>>");
-}
-
-
-/////////////////////////////////////////////////////////////////
-//
-// I2CM functions
-//
-/////////////////////////////////////////////////////////////////
-static uint8_t i2c_buffer[256];
-static void i2cm_readbyte(const struct cli_cmd_entry *pEntry)
-{
-	(void)pEntry;
-	// Add functionality here
-	uint32_t	i2c_addr;
-	uint32_t	reg_addr;
-
-	CLI_uint32_required( "i2c_addr", &i2c_addr );
-	CLI_uint32_required( "reg_addr", &reg_addr );
-
-	udma_i2cm_read(pEntry->cookie, (uint8_t)i2c_addr, (uint8_t)reg_addr, 1, i2c_buffer, false);
-	dbg_str_hex8("reg", (int)i2c_buffer[0]);
-}
-
-static void i2cm_writebyte(const struct cli_cmd_entry *pEntry)
-{
-	(void)pEntry;
-	// Add functionality here
-	uint32_t	i2c_addr;
-	uint32_t	reg_addr;
-	uint32_t	reg_value;
-
-	CLI_uint32_required( "i2c_addr", &i2c_addr );
-	CLI_uint32_required( "reg_addr", &reg_addr );
-	CLI_uint32_required( "reg_value", &reg_value );
-
-	i2c_buffer[0] = (uint8_t)reg_value;
-	udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, 1, i2c_buffer,  false);
-}
-
-static void i2cm_reset(const struct cli_cmd_entry *pEntry)
-{
-    (void)pEntry;
-    // Add functionality here
-
-    udma_i2cm_control(pEntry->cookie, kI2cmReset, NULL);
-}
-
-static void i2cm_singlebyte_test(const struct cli_cmd_entry *pEntry)
-{
-	(void)pEntry;
-	// Add functionality here
-	uint32_t	i2c_addr;
-	uint32_t	reg_addr;
-	bool		fPassed = false;
-
-	CLI_uint32_required( "i2c_addr", &i2c_addr );
-	CLI_uint32_required( "reg_addr", &reg_addr );
-
-	i2c_buffer[0] = 0xA5;
-	udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, 1, i2c_buffer,  false);
-	udma_i2cm_read(pEntry->cookie, (uint8_t)i2c_addr, (uint8_t)reg_addr, 1, i2c_buffer, false);
-	dbg_str_int("First access", i2c_buffer[0]);
-	if (i2c_buffer[0] == 0xA5) {
-		i2c_buffer[0] = 0x5A;
-		udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, 1, i2c_buffer,  false);
-		udma_i2cm_read(pEntry->cookie, (uint8_t)i2c_addr, (uint8_t)reg_addr, 1, i2c_buffer, false);
-		dbg_str_int("Second access", i2c_buffer[0]);
-		if (i2c_buffer[0] == 0x5A) {
-			fPassed = true;
-		}
-	}
-	if (fPassed) {
-		dbg_str("<<PASSED>>");
-	} else {
-		dbg_str("<<FAILED>>");
-	}
-}
-
-static void i2c_temp (const struct cli_cmd_entry *pEntry)
-{
-	char *message = 0;
-	int temp;
-	message  = pvPortMalloc(80);
-	configASSERT (message);
-	udma_i2cm_read(1, 0x96, 0x00, 2, i2c_buffer, false);
-	temp = (i2c_buffer[0] << 8) + i2c_buffer[1];
-	temp = ((temp *625) / 44000) + 32;
-	sprintf(message," Board temp = %d F\r\n", temp);
-	dbg_str(message);
-	vPortFree(message);
-}
-
-static void i2c_read_dev_id(const struct cli_cmd_entry *pEntry)
-{
-	char *message = 0;
-	int temp;
-	message  = pvPortMalloc(80);
-	configASSERT (message);
-	udma_i2cm_read(1, 0x96, 0x0B, 1, i2c_buffer, false);
-
-	sprintf(message," i2c_dev_id:= 0x%x \r\n", i2c_buffer[0]);
-	dbg_str(message);
-	if(i2c_buffer[0] == 0xCB) {
-		dbg_str("Dev Id Test: <<PASSED>>\r\n");
-	}
-	else {
-		dbg_str("Dev Id Test: <<FAILED>>\r\n");
-	}
-	vPortFree(message);
 }
