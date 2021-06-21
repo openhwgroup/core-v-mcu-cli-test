2d3ff26ab2a15512aba088bcca3e7a0cea9ab64c
Greg Martin <gmartin@quicklogic.com>
2021-06-21 12:16:47 -0500

    Added commandlone build script
    Signed-off-by: Greg Martin <gmartin@quicklogic.com>

---
+++ cli_test/cli_build.sh
@@ -0,0 +1,1 @@
+eclipse -nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data ~/eclipse-cli-workspace/ -import ~/git/core-v-mcu-cli-test/cli_test -cleanBuild cli_test/Default
