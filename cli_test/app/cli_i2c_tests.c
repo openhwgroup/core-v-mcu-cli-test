5bc453ca0c218b91970479f3ce1911a7e96ae1e0
someshwar_ms <someshwar.ms@optimuslogic.in>
2021-07-14 16:22:06 +0530

    Made changes in i2c tests to return correct status instead of returning no status.

--- cli_test/app/cli_i2c_tests.c
+++ cli_test/app/cli_i2c_tests.c
@@ -18,7 +18,7 @@
 #include "libs/cli/include/cli.h"
 #include "FreeRTOS.h"
 #include "semphr.h"	// Required for configASSERT
-
+#include "libs/utils/include/dbg_uart.h"
 
 
 // I2CM0 functions
@@ -35,7 +35,7 @@ const struct cli_cmd_entry i2cm0_functions[] =
 
   CLI_CMD_WITH_ARG( "readbyte", 	i2cm_readbyte,	0, "i2c_addr reg_addr 			-- read register" ),
   CLI_CMD_WITH_ARG( "writebyte", 	i2cm_writebyte,	0, "i2c_addr reg_addr value 	-- read register" ),
-  CLI_CMD_WITH_ARG( "singlebyte", 	i2cm_singlebyte_test,	0, "i2c_addr reg_addr	-- writes 0xA5 and then 0x5A to register and checks result" ),
+  CLI_CMD_WITH_ARG( "testsinglebyte", 	i2cm_singlebyte_test,	0, "i2c_addr reg_addr	-- writes 0xA5 and then 0x5A to register and checks result" ),
   CLI_CMD_TERMINATE()
 
 };
@@ -63,12 +63,35 @@ static void i2cm_readbyte(const struct cli_cmd_entry *pEntry)
 	// Add functionality here
 	uint32_t	i2c_addr;
 	uint32_t	reg_addr;
+	uint8_t 	lExpValTrueOrFalse = 0;
+	uint8_t    lExpVal = 0;
 
 	CLI_uint32_required( "i2c_addr", &i2c_addr );
 	CLI_uint32_required( "reg_addr", &reg_addr );
 
+	if( CLI_is_more_args() ){
+		lExpValTrueOrFalse = 1;
+		CLI_uint8_required("exp", &lExpVal);
+	}
+
 	udma_i2cm_read(pEntry->cookie, (uint8_t)i2c_addr, (uint8_t)reg_addr, 1, i2c_buffer, false);
 	dbg_str_hex8("reg", (int)i2c_buffer[0]);
+
+	if( lExpValTrueOrFalse )
+	{
+		if( i2c_buffer[0] == lExpVal )
+		{
+			dbg_str("<<PASSED>>\r\n");
+		}
+		else
+		{
+			dbg_str("<<FAILED>>\r\n");
+		}
+	}
+	else
+	{
+		dbg_str("<<DONE>>\r\n");
+	}
 }
 
 static void i2cm_writebyte(const struct cli_cmd_entry *pEntry)
@@ -85,6 +108,8 @@ static void i2cm_writebyte(const struct cli_cmd_entry *pEntry)
 
 	i2c_buffer[0] = (uint8_t)reg_value;
 	udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, 1, i2c_buffer,  false);
+
+	dbg_str("<<DONE>>");
 }
 
 static void i2cm_reset(const struct cli_cmd_entry *pEntry)
@@ -128,16 +153,14 @@ static void i2cm_singlebyte_test(const struct cli_cmd_entry *pEntry)
 
 static void i2c_temp (const struct cli_cmd_entry *pEntry)
 {
-	char *message = 0;
+
 	int temp;
-	message  = pvPortMalloc(80);
-	configASSERT (message);
+
 	udma_i2cm_read(1, 0x96, 0x00, 2, i2c_buffer, false);
 	temp = (i2c_buffer[0] << 8) + i2c_buffer[1];
 	temp = ((temp *625) / 44000) + 32;
-	sprintf(message," Board temp = %d F\r\n", temp);
-	dbg_str(message);
-	vPortFree(message);
+	CLI_printf(" Board temp = %d F\r\n", temp);
+	dbg_str("<<DONE>>");
 }
 
 static void i2c_read_dev_id(const struct cli_cmd_entry *pEntry)
