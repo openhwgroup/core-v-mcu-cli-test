3204d25091e9743cb60cbbf951b7fdff539499f8
someshwar_ms <someshwar.ms@optimuslogic.in>
2021-07-16 21:40:37 +0530

    Added mem test command to perform walking ones and pattern test on 32KB of memory after the executable code. Added multibyte read and write of i2c master.

--- cli_test/app/cli_i2c_tests.c
+++ cli_test/app/cli_i2c_tests.c
@@ -24,6 +24,8 @@
 // I2CM0 functions
 static void i2cm_readbyte(const struct cli_cmd_entry *pEntry);
 static void i2cm_writebyte(const struct cli_cmd_entry *pEntry);
+static void i2cm_writeMultiBytes(const struct cli_cmd_entry *pEntry);
+static void i2cm_readMultiBytes(const struct cli_cmd_entry *pEntry);
 static void i2cm_reset(const struct cli_cmd_entry *pEntry);
 static void i2c_temp(const struct cli_cmd_entry *pEntry);
 static void i2c_read_dev_id(const struct cli_cmd_entry *pEntry);
@@ -38,6 +40,8 @@ const struct cli_cmd_entry i2cm0_functions[] =
 
   CLI_CMD_WITH_ARG( "readbyte", 	i2cm_readbyte,	0, "i2c_addr reg_addr 			-- read register" ),
   CLI_CMD_WITH_ARG( "writebyte", 	i2cm_writebyte,	0, "i2c_addr reg_addr value 	-- read register" ),
+  CLI_CMD_WITH_ARG( "write", 	i2cm_writeMultiBytes,	0, "i2c_addr reg_addr value 	-- write register" ),
+  CLI_CMD_WITH_ARG( "read", 	i2cm_readMultiBytes,	0, "i2c_addr reg_addr value 	-- read register" ),
   CLI_CMD_WITH_ARG( "testsinglebyte", 	i2cm_singlebyte_test,	0, "i2c_addr reg_addr	-- writes 0xA5 and then 0x5A to register and checks result" ),
   CLI_CMD_SIMPLE ( "rbuff", i2c_buffer_reset,		    "reset i2c device application buffer"),
   CLI_CMD_TERMINATE()
@@ -50,6 +54,8 @@ const struct cli_cmd_entry i2cm1_functions[] =
 {
 	CLI_CMD_WITH_ARG( "readbyte", i2cm_readbyte,	1, "i2c_addr reg_addr 	-- read register" ),
 	CLI_CMD_WITH_ARG( "writebyte", 	i2cm_writebyte,	1, "i2c_addr reg_addr value 	-- read register" ),
+    CLI_CMD_WITH_ARG( "write", 	i2cm_writeMultiBytes,	1, "i2c_addr reg_addr value 	-- write register" ),
+    CLI_CMD_WITH_ARG( "read", 	i2cm_readMultiBytes,	1, "i2c_addr reg_addr value 	-- read register" ),
 	CLI_CMD_SIMPLE ( "temp", i2c_temp,				   "read on board temperature"),
 	CLI_CMD_SIMPLE ( "dev_id", i2c_read_dev_id,		    "read i2c device id"),
 	CLI_CMD_SIMPLE ( "rbuff", i2c_buffer_reset,		    "reset i2c device application buffer"),
@@ -113,6 +119,35 @@ static void i2cm_readbyte(const struct cli_cmd_entry *pEntry)
 	}
 }
 
+static void i2cm_readMultiBytes(const struct cli_cmd_entry *pEntry)
+{
+	(void)pEntry;
+	// Add functionality here
+	uint8_t	i2c_addr = 0;
+	uint8_t	reg_addr = 0;
+	uint8_t lNumOfBytesToRead = 0;
+	int i = 0;
+
+	CLI_uint8_required( "i2c_addr", &i2c_addr );
+	CLI_uint8_required( "reg_addr", &reg_addr );
+
+	CLI_uint8_required( "num of bytes to read", &lNumOfBytesToRead );
+
+	if( udma_i2cm_read(pEntry->cookie, (uint8_t)i2c_addr, (uint8_t)reg_addr, lNumOfBytesToRead, i2c_buffer, false) == pdTRUE )
+	{
+		dbg_str("Read value: \r\n");
+		for( i=0; i<lNumOfBytesToRead; i++ )
+		{
+			CLI_printf("[%d] 0x%02x\n", i, i2c_buffer[i]);
+		}
+	}
+	else
+	{
+		dbg_str("<<FAILED>>\r\n");
+	}
+}
+
+
 static void i2cm_writebyte(const struct cli_cmd_entry *pEntry)
 {
 	(void)pEntry;
@@ -127,6 +162,28 @@ static void i2cm_writebyte(const struct cli_cmd_entry *pEntry)
 
 	i2c_buffer[0] = (uint8_t)reg_value;
 	udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, 1, i2c_buffer,  false);
+
+	dbg_str("<<DONE>>");
+}
+
+static void i2cm_writeMultiBytes(const struct cli_cmd_entry *pEntry)
+{
+	(void)pEntry;
+	// Add functionality here
+	uint8_t	i2c_addr = 0;
+	uint8_t	reg_addr = 0;
+	int i = 0;
+
+	CLI_uint8_required( "i2c_addr", &i2c_addr );
+	CLI_uint8_required( "reg_addr", &reg_addr );
+	i = 0;
+	while( CLI_is_more_args() )
+	{
+		CLI_uint8_required("reg value", &i2c_buffer[i]);
+		i++;
+	}
+
+	udma_i2cm_write (pEntry->cookie, i2c_addr, reg_addr, i, i2c_buffer,  false);
 
 	dbg_str("<<DONE>>");
 }
