74563335b9914788c768e05d44367d9e74097a58
someshwar_ms <someshwar.ms@optimuslogic.in>
2022-01-26 17:21:41 +0530

    The uart bootloader was working, it was an issue with srec generation. Removed all the -j options in the application project cli_test to generate the srec. With this srec the application boots up. In the python script disabled the prints and added a percentage progress indicator.

--- cli_test/serialPort.py
+++ cli_test/serialPort.py
@@ -14,10 +14,15 @@ else:
         serialPort = serial.Serial(port = sys.argv[1], baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
         serialString = ""                           # Used to hold data coming over UART
-        print("Opening " + sys.argv[2])
-        srecfile = open(sys.argv[2], 'r')
         
-        #line = srecfile.readline()
+        srecfile = open(sys.argv[2], 'r')
+        print("Opened " + sys.argv[2] + " successfully ! ")
+        TotalNumOfLines = len(srecfile.readlines())
+        print('Total lines:', TotalNumOfLines)
+        lineCount = 0;
+
+        srecfile.seek(0)
+        #line = srecfile.readline() 
         #print(line.encode(), end = "")
         #line = srecfile.readline()
         #print(line, end = "")
@@ -31,15 +36,19 @@ else:
                 serialString = serialPort.readline()
 
                 # Print the contents of the serial data
-                print(serialString.decode('Ascii'), end = "")
+                #print(serialString.decode('Ascii'), end = "")
                 if( (serialString.decode('Ascii') == "A2 BOOTME\r\n") or (serialString.decode('Ascii') == "OK\r\n" ) ):
                     line = srecfile.readline()
                     serialPort.write(line.encode())
-                    print(line, end = "")
-                    if not line:
-                        print("File read complete")
+                    lineCount += 1
+                    progressPercentage = (lineCount / TotalNumOfLines ) * 100
+                    print("%d / %d          [%d%%]" % (lineCount, TotalNumOfLines, progressPercentage), end = '\r')
+                    if (lineCount == TotalNumOfLines):
+                        print("Done! Exiting.")
                         srecfile.close()
+                        serialPort.close()
+                        break
                 # Tell the device connected over the serial port that we recevied the data!
                 # The b at the beginning is used to indicate bytes!
                 #serialPort.write(b"Thank you for sending data \r\n")
-
+        exit()
